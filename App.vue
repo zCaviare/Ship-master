70ab2d712986eae96dd2585feb7f1bad8a3b8405
谈培正 <tanpeizheng@itonghui.org>
2022-03-18 15:52:10 +0800

    修改sqlit逻辑

--- App.vue
+++ App.vue
@@ -3,35 +3,6 @@
   export default {
     onLaunch: function() {
       console.log('App Launch')
-      plus.sqlite.openDatabase({
-        name: 'first',
-        path: '_doc/ship.db',
-        success: function(e) {
-          console.log('打开数据库ship.db成功');
-          plus.sqlite.executeSql({
-            name: 'first',
-            sql: 'create table ip_port("id" INT(32),"ip" CHAR(110),"port" INT(10))',
-            success: function(e) {
-              plus.sqlite.executeSql({
-                name: 'first',
-                sql: "insert into ip_port values(1,'192.168.0.7',23)",
-                success: function(e) {
-                  console.log('创建表ip_port和插入数据成功');
-                },
-                fail: function(e) {
-                  console.log('创建表ip_port成功但插入数据失败: ' + JSON.stringify(e));
-                }
-              });
-            },
-            fail: function(e) {
-              console.log('创建表ip_port失败: ' + JSON.stringify(e));
-            }
-          });
-        },
-        fail: function(e) {
-          console.log('打开数据库ship.db失败');
-        }
-      });
     },
     onShow: function() {
       console.log('App Show')
